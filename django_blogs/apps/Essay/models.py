ed57c08d446362f57ccf54ce3de7a5d4c97d32b7
yao <2521929494@qq.com>
2022-06-15 21:03:30 +0800

    数据更新

--- django_blogs/apps/Essay/models.py
+++ django_blogs/apps/Essay/models.py
@@ -3,6 +3,13 @@ from django.db import models
 
 # Create your models here.
 # 分类数据库
-class Classify(models.Model):
+class ClassifyModel(models.Model):
     Classify_Id = models.AutoField(primary_key=True)  # 分类ID
-    Classify_Name = models.CharField(max_length=50,blank=False,unique=True)
+    Classify_Name = models.CharField(max_length=50, blank=False, unique=True)  # 分类名称
+
+
+# 标签库
+class LabelModel(models.Model):
+    Label_Id = models.AutoField(primary_key=True)  # 标签ID
+    Label_Color = models.CharField(max_length=10, blank=False, unique=True)  # 标签颜色
+    Label_Name = models.CharField(max_length=50, blank=False, unique=True)  # 标签名称
--- django_blogs/apps/Essay/urls.py
+++ django_blogs/apps/Essay/urls.py
@@ -2,6 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path(r'classify/get/', views.GetClassifyView.as_view()),
-    path(r'classify/del/', views.DelClassifyView.as_view()),
+    path(r'classify/get/', views.GetClassifyView.as_view()),  # 查询分类
+    path(r'classify/del/', views.DelClassifyView.as_view()),  # 删除分类
+    path(r'classify/add/', views.AddClassifyView.as_view()),  # 添加分类
+    path(r'label/get/', views.GetLabelView.as_view()),  # 查询分类
+    path(r'label/del/', views.DelLabelView.as_view()),  # 删除分类
+    path(r'label/add/', views.AddLabelView.as_view()),  # 添加分类
 ]
